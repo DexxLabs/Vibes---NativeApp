c3d084794546f52281fa30b08a127b4d32233072
DexxLabs <ranbeersingh0520@gmail.com>
2024-05-18 16:01:34 +0530

    alpha release:0.0.1

---
+++ resources/HeaderFile.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function HeaderFile() {
+  return (
+    <View>
+      <Text>HeaderFile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
